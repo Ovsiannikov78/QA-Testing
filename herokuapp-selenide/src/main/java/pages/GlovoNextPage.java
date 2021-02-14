709e1d59fde154598968d64ccfffa09baf437eeb
Ovsiannikov78 <seri_tanya@mail.ru>
2021-02-14 13:25:50 +0100

    previous HW - Glovo registration form

---
+++ herokuapp-selenide/src/main/java/pages/GlovoNextPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+
+public class GlovoNextPage {
+
+    @FindBy(xpath = "//div[@id='app']/section/section/div/p")
+    public SelenideElement confirmationText;
+
+    public SelenideElement successfulRegistration() {
+        return confirmationText;
+    }
+
+}
